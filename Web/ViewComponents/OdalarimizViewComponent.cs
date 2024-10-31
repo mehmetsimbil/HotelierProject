0f57491fff7965633eedd4f9f7855e2d7ed7d332
KK <KK@DESKTOP-2LGJ839>
2024-10-31 16:53:17 +0300

    Little changes

---
+++ Web/ViewComponents/OdalarimizViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using HotelProject.Business.Abstracts;
+using HotelProject.Business.Requests.Room;
+using HotelProject.Business.Responses.Room;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.ViewComponents
+{
+    public class OdalarimizViewComponent : ViewComponent
+    {
+        private readonly IRoomService _roomService;
+
+        public OdalarimizViewComponent(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var req = new GetRoomListRequest();
+            GetRoomListResponse result = _roomService.GetList(req);
+            var model = result.Items;
+            return View(model);
+
+        }
+    }
+}
