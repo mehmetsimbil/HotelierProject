bee602fdc638302e24ae0001ecb901f866d7960e
KK <KK@DESKTOP-2LGJ839>
2024-11-02 13:45:24 +0300

    Last changed

---
+++ Web/ViewComponents/ServislerimizViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using HotelProject.Business.Abstracts;
+using HotelProject.Business.Requests.Service;
+using HotelProject.Business.Responses.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.ViewComponents
+{
+    public class ServislerimizViewComponent : ViewComponent
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServislerimizViewComponent(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var req = new GetServiceListRequest();
+            GetServiceListResponse result = _serviceService.GetList(req);
+            var model = result.Items;
+            return View(model);
+
+        }
+    }
+}
